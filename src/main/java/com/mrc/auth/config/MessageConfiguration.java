7ce774b856f948e743c419cdf021cedbbac39da4
jeonsanghoon <mrc0700@gmail.com>
2021-03-15 18:01:46 +0900

    api 관련 예제 소스 만드는 중 메세지 처리 완료

--- src/main/java/com/mrc/auth/config/MessageConfiguration.java
+++ src/main/java/com/mrc/auth/config/MessageConfiguration.java
@@ -17,18 +17,14 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 @Configuration
-
 public class MessageConfiguration implements WebMvcConfigurer {
 
-
     @Bean // 세션에 지역설정. default는 KOREAN = 'ko'
-
     public LocaleResolver localeResolver() {
         SessionLocaleResolver slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.KOREAN);
         return slr;
     }
-
 
     @Bean // 지역설정을 변경하는 인터셉터. 요청시 파라미터에 lang 정보를 지정하면 언어가 변경됨.
     public LocaleChangeInterceptor localeChangeInterceptor() {
@@ -37,19 +33,16 @@ public class MessageConfiguration implements WebMvcConfigurer {
         return lci;
     }
 
-
     @Override // 인터셉터를 시스템 레지스트리에 등록
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(localeChangeInterceptor());
     }
 
-
     @Bean // yml 파일을 참조하는 MessageSource 선언
     public MessageSource messageSource(
-        @Value("${spring.messages.basename}") String basename,
-        @Value("${spring.messages.encoding}") String encoding
-    )
-    {
+            @Value("${spring.messages.basename}") String basename,
+            @Value("${spring.messages.encoding}") String encoding
+    ) {
         YamlMessageSource ms = new YamlMessageSource();
         ms.setBasename(basename);
         ms.setDefaultEncoding(encoding);
@@ -59,9 +52,7 @@ public class MessageConfiguration implements WebMvcConfigurer {
         return ms;
     }
 
-
     // locale 정보에 따라 다른 yml 파일을 읽도록 처리
-
     private static class YamlMessageSource extends ResourceBundleMessageSource {
         @Override
         protected ResourceBundle doGetBundle(String basename, Locale locale) throws MissingResourceException {
@@ -69,4 +60,3 @@ public class MessageConfiguration implements WebMvcConfigurer {
         }
     }
 }
-
